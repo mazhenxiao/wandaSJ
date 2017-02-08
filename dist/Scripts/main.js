991e998a98b38ba91e0012093dfc814f9a19e2ae
mazhenxiao@gmail.com <mazhenxiao@gmail.com>
2017-02-08 11:11:50 +0800

    test

---
+++ dist/Scripts/main.js
@@ -0,0 +1,11 @@
+;(function($,window){
+
+    var publicFun = {
+         
+    }
+   
+
+    iss.tabs(); //翻页标签初始化
+    iss.tableodd(".iss-tableodd")
+  
+})(jQuery,window)
---
+++ js/webpack.config.js
@@ -0,0 +1,3 @@
+/**
+ * Created by mazhenxiao on 2017/2/8.
+ */
